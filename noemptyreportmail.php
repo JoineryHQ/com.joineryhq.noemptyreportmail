c9453bcc27835594c0a06e29d4ae4dd527de5364
Allen Shaw <allen@JoineryHQ.com>
2023-10-01 16:34:09 -0500

    civix upgrade v23.02.1.

--- noemptyreportmail.php
+++ noemptyreportmail.php
@@ -112,59 +112,12 @@ function noemptyreportmail_civicrm_install(): void {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function noemptyreportmail_civicrm_postInstall(): void {
-  _noemptyreportmail_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function noemptyreportmail_civicrm_uninstall(): void {
-  _noemptyreportmail_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function noemptyreportmail_civicrm_enable(): void {
   _noemptyreportmail_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function noemptyreportmail_civicrm_disable(): void {
-  _noemptyreportmail_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function noemptyreportmail_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _noemptyreportmail_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function noemptyreportmail_civicrm_entityTypes(&$entityTypes): void {
-  _noemptyreportmail_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
